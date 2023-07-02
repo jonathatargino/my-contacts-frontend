914b68c61a98171e2a675da29db74820f3bb8e19
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-07-02 12:04:07 -0300

    created ContactCard component

---
+++ src/app/components/ContactCard/index.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import Image from "next/image";
+import { pencilIcon, trashIcon } from "@/assets/images";
+import { IContact } from "@/provider/contact";
+
+export default function ContactCard({ id, name, category, email, phone }: IContact) {
+  return (
+    <div className="flex items-center justify-between bg-white p-4">
+      <div>
+        <div>
+          <strong>{name}</strong>
+          <span className="ml-2 rounded-[4px] bg-primary-lighter p-1 text-xs font-bold uppercase text-primary-main">
+            {category}
+          </span>
+        </div>
+
+        <span className="block text-gray-200">{email}</span>
+        <span className="block text-gray-200">{phone}</span>
+      </div>
+
+      <div className="flex gap-2">
+        <Link href={`/edit/${id}`}>
+          <Image src={pencilIcon} alt="Editar contato" />
+        </Link>
+
+        <button>
+          <Image src={trashIcon} alt="Deletar contato" />
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ src/provider/contact/index.ts
@@ -0,0 +1,7 @@
+export interface IContact {
+  id: string;
+  name: string;
+  category: string;
+  email: string;
+  phone: string;
+}
