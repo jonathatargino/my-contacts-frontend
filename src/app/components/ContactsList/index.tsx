7631014efbb93c6437b2d926bb1cd1d4823948f9
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-07-07 10:03:54 -0300

    filtering contacts by search input, fixed -> loading UI called in every state update in Home

--- src/app/components/ContactsList/index.tsx
+++ src/app/components/ContactsList/index.tsx
@@ -1,18 +1,30 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 import { arrowIcon } from "@/assets/images";
 import ContactCard from "../ContactCard";
 import { IContact } from "@/provider/contact";
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import classNames from "classnames";
 
 interface ContactsListProps {
-  contacts: Array<IContact>;
+  ascendentOrderContacts: Array<IContact>;
   descendentOrderContacts: Array<IContact>;
 }
 
-export default async function ContactsList({ contacts, descendentOrderContacts }: ContactsListProps) {
+export default function ContactsList({ ascendentOrderContacts, descendentOrderContacts }: ContactsListProps) {
   const [isInDescendentOrder, setDescendentOrder] = useState<boolean>(false);
+  const [searchInputValue, setSearchInputValue] = useState<string>("");
+
+  const contacts = useMemo(() => {
+    return isInDescendentOrder ? descendentOrderContacts : ascendentOrderContacts;
+  }, [isInDescendentOrder]);
+
+  const filteredContacts = useMemo(
+    () => contacts.filter((contact) => RegExp(searchInputValue.toUpperCase()).test(contact.name.toUpperCase())),
+    [searchInputValue, contacts],
+  );
 
   const toggleDescendentOrder = useCallback(() => {
     setDescendentOrder((prevState) => !prevState);
@@ -23,6 +35,8 @@ export default async function ContactsList({ contacts, descendentOrderContacts }
       <input
         className="h-[50px] w-full rounded-3xl bg-white p-4 shadow-soft placeholder:text-gray-200"
         placeholder="Pesquise um contato..."
+        value={searchInputValue}
+        onChange={(e) => setSearchInputValue(e.target.value)}
       />
 
       <header className="flex items-center justify-between">
@@ -46,33 +60,22 @@ export default async function ContactsList({ contacts, descendentOrderContacts }
             <Image
               src={arrowIcon}
               alt={isInDescendentOrder ? "Ordenar crescentemente" : "Ordenar decrescentemente"}
-              className={classNames({ "rotate-180 transition-all": isInDescendentOrder })}
+              className={classNames("transition-all", { "rotate-180": isInDescendentOrder })}
             ></Image>
           </button>
         </header>
 
         <div className="flex flex-col gap-4">
-          {isInDescendentOrder
-            ? descendentOrderContacts.map((contact) => (
-                <ContactCard
-                  key={contact.id}
-                  id={contact.id}
-                  email={contact.email}
-                  category_name={contact.category_name}
-                  name={contact.name}
-                  phone={contact.phone}
-                />
-              ))
-            : contacts.map((contact) => (
-                <ContactCard
-                  key={contact.id}
-                  id={contact.id}
-                  email={contact.email}
-                  category_name={contact.category_name}
-                  name={contact.name}
-                  phone={contact.phone}
-                />
-              ))}
+          {filteredContacts.map((contact) => (
+            <ContactCard
+              key={contact.id}
+              id={contact.id}
+              email={contact.email}
+              category_name={contact.category_name}
+              name={contact.name}
+              phone={contact.phone}
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-"use client";
-
 import { IContact } from "@/provider/contact";
 import { useFetch } from "@/hooks/useFetch";
 
 import ContactsList from "./components/ContactsList";
 
 export default async function Home() {
-  const getContacts = useFetch<IContact[]>({ endpoint: "contacts", method: "GET" });
-  const contacts = await getContacts();
+  const getAscendentOrderContacts = useFetch<IContact[]>({ endpoint: "contacts", method: "GET" });
+  const ascendentOrderContacts = await getAscendentOrderContacts();
 
   const getDescendentOrderContacts = useFetch<IContact[]>({ endpoint: "contacts?order=desc", method: "GET" });
   const descendentOrderContacts = await getDescendentOrderContacts();
 
-  return <ContactsList contacts={contacts} descendentOrderContacts={descendentOrderContacts} />;
+  return (
+    <ContactsList ascendentOrderContacts={ascendentOrderContacts} descendentOrderContacts={descendentOrderContacts} />
+  );
 }
