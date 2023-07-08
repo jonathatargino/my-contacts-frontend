f3a0ef208ee38fc15c04cdbbeac5b3b913b45a97
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-07-08 14:37:56 -0300

    none contacts message

--- src/app/components/ContactsList/index.tsx
+++ src/app/components/ContactsList/index.tsx
@@ -2,11 +2,12 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { arrowIcon } from "@/assets/images";
+import { arrowIcon, emptyBoxIcon } from "@/assets/images";
 import ContactCard from "../ContactCard";
 import { IContact } from "@/provider/contact";
 import { useCallback, useMemo, useState } from "react";
 import classNames from "classnames";
+import { twMerge } from "tailwind-merge";
 
 interface ContactsListProps {
   ascendentOrderContacts: Array<IContact>;
@@ -18,6 +19,7 @@ export default function ContactsList({ ascendentOrderContacts, descendentOrderCo
   const [searchInputValue, setSearchInputValue] = useState<string>("");
 
   const contacts = isInDescendentOrder ? descendentOrderContacts : ascendentOrderContacts;
+  const isContactListNotEmpty = contacts.length > 0;
 
   const filteredContacts = useMemo(
     () => contacts.filter((contact) => contact.name.toUpperCase().includes(searchInputValue.toUpperCase())),
@@ -29,25 +31,38 @@ export default function ContactsList({ ascendentOrderContacts, descendentOrderCo
   }
 
   return (
-    <div className="flex flex-col gap-8">
-      <input
-        className="h-[50px] w-full rounded-3xl bg-white p-4 shadow-soft placeholder:text-gray-200"
-        placeholder="Pesquise um contato..."
-        value={searchInputValue}
-        onChange={(e) => setSearchInputValue(e.target.value)}
-      />
+    <>
+      <div className="mb-4 flex flex-col gap-8">
+        {isContactListNotEmpty ? (
+          <input
+            className="h-[50px] w-full rounded-3xl bg-white p-4 shadow-soft placeholder:text-gray-200"
+            placeholder="Pesquise um contato..."
+            value={searchInputValue}
+            onChange={(e) => setSearchInputValue(e.target.value)}
+          />
+        ) : null}
 
-      <header className="flex items-center justify-between">
-        <strong className="text-2xl">
-          {filteredContacts.length} {filteredContacts.length === 1 ? "contato" : "contatos"}
-        </strong>
-        <Link
-          href="/add"
-          className="border-2 border-primary-main p-3 font-bold text-primary-main transition-colors hover:bg-primary-main hover:text-white"
+        <header
+          className={twMerge(
+            classNames("flex items-center justify-between border-b-2 border-gray-200 border-opacity-20 pb-4", {
+              "justify-center": !isContactListNotEmpty,
+            }),
+          )}
         >
-          Novo contato
-        </Link>
-      </header>
+          {isContactListNotEmpty ? (
+            <strong className="text-2xl">
+              {filteredContacts.length} {filteredContacts.length === 1 ? "contato" : "contatos"}
+            </strong>
+          ) : null}
+
+          <Link
+            href="/add"
+            className="rounded border-2 border-primary-main px-4 py-3 font-bold text-primary-main transition-colors hover:bg-primary-main hover:text-white"
+          >
+            Novo contato
+          </Link>
+        </header>
+      </div>
 
       <div>
         {filteredContacts.length > 0 ? (
@@ -62,20 +77,30 @@ export default function ContactsList({ ascendentOrderContacts, descendentOrderCo
             </button>
           </header>
         ) : null}
-
-        <div className="flex flex-col gap-4">
-          {filteredContacts.map((contact) => (
-            <ContactCard
-              key={contact.id}
-              id={contact.id}
-              email={contact.email}
-              category_name={contact.category_name}
-              name={contact.name}
-              phone={contact.phone}
-            />
-          ))}
-        </div>
+        {isContactListNotEmpty ? (
+          <div className="flex flex-col gap-4">
+            {filteredContacts.map((contact) => (
+              <ContactCard
+                key={contact.id}
+                id={contact.id}
+                email={contact.email}
+                category_name={contact.category_name}
+                name={contact.name}
+                phone={contact.phone}
+              />
+            ))}
+          </div>
+        ) : (
+          <div className="flex flex-col items-center justify-center gap-2">
+            <Image src={emptyBoxIcon} alt="caixa vazia" />
+            <p className="text-center text-gray-200">
+              Você ainda não tem nenhum contato cadastrado! Clique no botão
+              <strong className="text-primary-main"> &quot;Novo contato&quot; </strong>
+              acima para cadastrar o seu primeiro!
+            </p>
+          </div>
+        )}
       </div>
-    </div>
+    </>
   );
 }
--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -3,3 +3,4 @@ export { default as logoIcon } from "./logo.svg";
 export { default as pencilIcon } from "./pencil.svg";
 export { default as trashIcon } from "./trash.svg";
 export { default as sadIcon } from "./sad.svg";
+export { default as emptyBoxIcon } from "./empty-box.svg";
