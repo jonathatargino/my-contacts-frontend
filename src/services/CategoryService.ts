efca8287a1df5f88eaee25a80308e090e2e0cde0
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-07-10 22:17:29 -0300

    created CategoryService with getAll and getById functions

---
+++ src/services/CategoryService.ts
@@ -0,0 +1,23 @@
+import { ICategory } from "@/provider/category";
+import HttpClient from "./utils/HttpClient";
+
+class CategoryService {
+  httpClient: HttpClient;
+  baseEndpoint: string;
+
+  constructor() {
+    const baseURL = process.env.NEXT_PUBLIC_API_URL as string;
+    this.httpClient = new HttpClient(baseURL);
+    this.baseEndpoint = "/categories";
+  }
+
+  getAll(order: "asc" | "desc" = "asc") {
+    return this.httpClient.get<ICategory[]>(`${this.baseEndpoint}?order=${order}`);
+  }
+
+  getById(id: string) {
+    return this.httpClient.get<ICategory>(`${this.baseEndpoint}/${id}`);
+  }
+}
+
+export default new CategoryService();
