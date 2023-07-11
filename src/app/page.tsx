34aaf8be8332c06a41faac2cd2111f167dea4dcf
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-07-10 22:19:58 -0300

    change useFetch to ContactService in initial page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
-import { IContact } from "@/provider/contact";
-import { useFetch } from "@/hooks/useFetch";
-
 import ContactsList from "./components/ContactsList";
+import ContactService from "@/services/ContactService";
 
 export default async function Home() {
-  const getAscendentOrderContacts = useFetch<IContact[]>({ endpoint: "contacts", method: "GET" });
-  const ascendentOrderContacts = await getAscendentOrderContacts();
-
-  const getDescendentOrderContacts = useFetch<IContact[]>({ endpoint: "contacts?order=desc", method: "GET" });
-  const descendentOrderContacts = await getDescendentOrderContacts();
+  const ascendentOrderContacts = await ContactService.getAll();
+  const descendentOrderContacts = await ContactService.getAll("desc");
 
   return (
     <ContactsList ascendentOrderContacts={ascendentOrderContacts} descendentOrderContacts={descendentOrderContacts} />
