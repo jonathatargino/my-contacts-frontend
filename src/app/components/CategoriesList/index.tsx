f89ae496b89a6bb7a3c4e4950f9e2353abee9063
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-07-20 18:32:22 -0300

    CategoriesList grid have 1 col in small devices and 2 in bigger devices

--- src/app/components/CategoriesList/index.tsx
+++ src/app/components/CategoriesList/index.tsx
@@ -15,7 +15,7 @@ export default function CategoriesList({ ascendentOrderCategories, descendentOrd
       Card={CategoryCard}
       ascendentOrderItems={ascendentOrderCategories}
       descendentOrderItems={descendentOrderCategories}
-      CardListStyles="grid grid-cols-2 gap-x-4 gap-y-2"
+      CardListStyles="grid grid-cols-1 sm:grid-cols-2 gap-x-4 gap-y-2"
     />
   );
 }
