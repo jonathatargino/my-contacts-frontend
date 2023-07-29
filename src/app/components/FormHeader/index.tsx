ddaad166531c2dd86bfe2224244219ea823c7f3d
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-07-29 00:57:29 -0300

    fix -> contacts and categories not updating after add forms

--- src/app/components/FormHeader/index.tsx
+++ src/app/components/FormHeader/index.tsx
@@ -1,25 +1,30 @@
 "use client";
 
-import Link from "next/link";
 import Image from "next/image";
 import { arrowIcon } from "@/assets/images";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 interface FormHeaderProps {
   title: string;
 }
 
 export default function FormHeader({ title }: FormHeaderProps) {
+  const router = useRouter();
+
   const splittedPathName = usePathname().split("/");
   const previousPathName = `/${splittedPathName[1]}`;
-  console.log(previousPathName);
+
+  function handleBackButtonClick() {
+    router.push(previousPathName);
+    router.refresh();
+  }
 
   return (
     <header>
-      <Link href={previousPathName} className="mb-2 flex gap-2">
+      <button onClick={handleBackButtonClick} className="mb-2 flex gap-2">
         <Image src={arrowIcon} alt="" className="-rotate-90" />
         <span className="font-bold text-primary-main">Voltar</span>
-      </Link>
+      </button>
       <h1 className="mb-6 text-2xl font-bold">{title}</h1>
     </header>
   );
