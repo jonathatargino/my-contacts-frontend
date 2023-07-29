5af06eeda377ce440ecaa010adf35918f0c9d2c1
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-07-29 00:52:37 -0300

    fixed modal layout when havemassive words

--- src/app/components/Modal/ModalBody.tsx
+++ src/app/components/Modal/ModalBody.tsx
@@ -3,7 +3,10 @@ import { twMerge } from "tailwind-merge";
 
 export default function ModalBody({ children, className, ...props }: HTMLAttributes<HTMLDivElement>) {
   return (
-    <div {...props} className={twMerge("w-4/5 max-w-[450px] rounded bg-white p-6 shadow-soft", className)}>
+    <div
+      {...props}
+      className={twMerge("w-4/5 max-w-[450px] rounded bg-white p-6 shadow-soft [word-wrap:break-word]", className)}
+    >
       {children}
     </div>
   );
